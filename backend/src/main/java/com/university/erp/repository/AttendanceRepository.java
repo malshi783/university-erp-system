08c31ba7288ba394a63a7de04a3d4a9419942e9f
Sachini Senarathna <sachinisenarathnans1007@gmail.com>
2026-07-01 23:26:17 +0530

    Login page

--- backend/src/main/java/com/university/erp/repository/AttendanceRepository.java
+++ backend/src/main/java/com/university/erp/repository/AttendanceRepository.java
@@ -1,4 +1,13 @@
 package com.university.erp.repository;
 
-public interface AttendanceRepository {
+import com.university.erp.model.Attendance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+    List<Attendance> findByStudentRegNo(String studentRegNo);
+    List<Attendance> findByCourseCode(String courseCode);
 }
