572b5cca18911e4950bf0dc88d0a11233210870e
Sachini Senarathna <sachinisenarathnans1007@gmail.com>
2026-07-01 12:04:47 +0530

    added attendance model and repository

--- backend/src/main/java/com/university/erp/model/Attendence.java
+++ backend/src/main/java/com/university/erp/model/Attendence.java
@@ -1,4 +1,20 @@
 package com.university.erp.model;
 
-public class Attendence {
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "attendence")
+@Data
+class Attendence {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String studentRegNo;
+    private String courseCode;
+    private LocalDate date;
+    private String status;
 }
